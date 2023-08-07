17b0e68352b4e213bf8c1fd91e814e5e7e60c25b
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2023-08-07 18:41:20 -0300

    feat: implementação de método que obtém dados históricos de um ativo

--- stock_data.py
+++ stock_data.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from flask import Flask, jsonify
 import json
+import numpy as np
+import yfinance as yf
 
 app = Flask(__name__)
 
@@ -19,6 +21,24 @@ def get_tickers():
         mimetype='application/json'
     )
     return response
+
+@app.route('/historicaldata/stock/<string:ticker>', methods=['GET'])
+def get_historical_data(ticker):
+    print(ticker)
+    try:
+        # Faz o download dos dados históricos da ação usando o yfinance
+        df = yf.download(ticker, start='2015-01-01')
+        # Transforma o DataFrame em um dicionário
+        historical_data = df.to_dict(orient='records')
+        # Retorna a resposta JSON diretamente usando a função jsonify do Flask
+        return jsonify(historical_data)
+
+    except Exception as e:
+        # Em caso de erro, retorna uma resposta de erro
+        error_response = {
+            'error': str(e)
+        }
+        return jsonify(error_response), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
